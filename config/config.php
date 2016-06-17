fd757e456c5593ad8af9385f8493492b7447a5b0
chizhov-and <chizhov.and@gmail.com>
2016-06-17 17:25:49 +0300

    Saved full lesson - site on mvc

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+Config::set('site_name','WEB-portal');
+Config::set('languages',array('en','fr'));
+
+// Routes. Route name => method prefix
+
+Config::set('routes',array(
+    'default' => '',
+    'admin' => 'admin_',
+));
+
+Config::set('default_route', 'default');
+Config::set('default_language', 'en');
+Config::set('default_controller', 'pages');
+Config::set('default_action', 'index');
+
+Config::set('db.host','localhost' );
+Config::set('db.user','root' );
+Config::set('db.password','' );
+Config::set('db.db_name','mvc' );
+Config::set('salt','jQveY86RSUwa5TzPZ7cg' );
---
+++ controllers/contacts.controller.php
@@ -0,0 +1,25 @@
+<?php
+
+class ContactsController extends Controller
+{
+
+    public function __construct($data = array())
+    {
+        parent::__construct($data);
+        $this->model = new Message();
+    }
+
+    public function index()
+    {
+        if ($_POST){
+            if ($this->model->save($_POST)){
+                Session::setFlash('Thank yoy! Your message was sent successfully!');
+            }
+        }
+    }
+
+    public function admin_index()
+    {
+        $this->data = $this->model->getList();
+    }
+}
